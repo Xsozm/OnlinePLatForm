f878fafd419059be576b8b5f803a2f7ca1901ef4
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-06 01:45:23 +0200

    Models Added

---
+++ models/Request.js
@@ -0,0 +1,35 @@
+let mongoose = require('mongoose');
+let RequestSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	name: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	mob: {
+		type: Number,
+		required: true,
+	},
+	address:{
+		type :String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+
+	},
+	status: {
+		type: String, default: 'Not Reviewed'
+	}
+
+
+});
+let Request = mongoose.model('Request', RequestSchema);
+module.exports = Request;
---
+++ models/User.js
@@ -0,0 +1,29 @@
+let mongoose = require('mongoose');
+let UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role:{
+		type :Number,
+		required: true
+	},
+	address: {
+		type: String,
+	}
+
+});
+let User = mongoose.model('User', UserSchema);
+module.exports = User;
