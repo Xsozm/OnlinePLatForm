7bf34ec83aec0c2b307b67767a14f6854a15d0cc
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-05 23:08:25 +0200

    Start App-ENV

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
+const mongoose = require('mongoose')
+const db = mongoose.createConnection(process.env.CONNECTION_STRING );
+const PORT = process.env.PORT || 3000;;
 
+db.once('connected', function (err) {
+	if (err) { return console.error(err) }
+	else
+		console.log("connected");
 
+})
+
+app.listen(PORT, () => {
+	console.log('app listening on port 3000!');
+});
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-	console.log('app listening on port 3000!');
-});
+
