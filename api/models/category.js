651d9b2cd7608245ce8dbd30174ea4ec778b5021
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2018-11-05 22:10:20 +0200

    add category & subcategory schemas

---
+++ api/models/category.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CategorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    subCategories: [{subCategoryId:String}],
+    created_at: {
+      type: Date,
+      required: true
+    },
+    updated_at: {
+      type: Date,
+      required: true
+    },
+});
+
+mongoose.model('Category', CategorySchema);
