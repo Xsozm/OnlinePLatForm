8a84e9541ab4d8f50181f2cada1cd9f9608caec1
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-05 22:22:09 +0200

    Start app

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(12);
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+	console.log('app listening on port 3000!');
+});
