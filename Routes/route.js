ef1973e17942b720c5170b721ae9961ab6df8ad5
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2018-11-12 23:07:58 +0200

    add create product endpoint

--- Routes/route.js
+++ Routes/route.js
@@ -1,12 +1,13 @@
 // require dependincies
 var express = require('express');
-var router = express.Router();
+var router = express.Router(),
+porductController = require('../Controllers/product.controller.js');
 
 
 
 // add routes
 
-
+router.post('/product/create',productController.createProduct);
 
 
 module.exports = router;
