519466fcd8c727ec0d4c29e812ceebbb3a7a6c20
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2018-12-01 16:18:17 +0200

    add documentation

--- Routes/route.js
+++ Routes/route.js
@@ -35,10 +35,21 @@ router.put('/product/:productID',porductController.updateProduct);
 router.get('/products/:productid' , cart.ViewProducts);
 router.get('/products/:productid/:sample' , cart.AddingProductTOCart);
 
-
+// Posts a subCategory
+// For the JSON body check the schema in the models folder
 router.post('/subcategory/create',SubCategoryController.create);
+
+// Find all products belonging to a subCategory with the id specicfied in the URL
 router.get('/subcategory/findProductsBySubCategory/:subCategoryID',SubCategoryController.findProductsBySubCategory);
+
+// Finds all the products belonging to a subCategory with the id specicfied in the URL
+// Within a range of prices
+// Takes 2 paramaeters minPrice & maxPrice, both are numbers
 router.get('/subcategory/findProductsByPrice/:subCategoryID',SubCategoryController.findProductsByPrice);
+
+// Finds all the products belonging to a subCategory with the id specicfied in the URL
+// Within a range of ratings
+// Takes 2 paramaeters minRate & maxRate, both are numbers
 router.get('/subcategory/findProductsByRating/:subCategoryID',SubCategoryController.findProductsByRating);
 
 router.get('/admin/viewRequests/',AdminController.ViewRequests);
