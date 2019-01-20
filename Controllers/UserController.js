7ce251f7c72b1c1751dd88bef4a00cd15d926827
Xsozm <hazem29778865@gmail.com>
2019-01-20 20:12:45 +0200

    BugsFixed

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -140,112 +140,106 @@ module.exports = {
 });
 },
 
-changepass:function(req,res){
-  User.findOne({ email: req.body.email }, function(err, user) {
-    if (!user) {
-      // notfound
-    }
-    var Salt = crypto.randomBytes(16).toString('hex');
-    var Hash = crypto.pbkdf2Sync(req.body.password, Salt, 1000, 64, 'sha512').toString('hex');
-    user.salt = Salt;
-    user.hash = Hash;
-    user.save(function(err){
+changepass:function(req,res) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if (!user) {
+            // notfound
+        }
+        var Salt = crypto.randomBytes(16).toString('hex');
+        var Hash = crypto.pbkdf2Sync(req.body.password, Salt, 1000, 64, 'sha512').toString('hex');
+        user.salt = Salt;
+        user.hash = Hash;
+        user.save(function (err) {
 
+        });
     })
-}
+},
 
 
-
-}
-
-module.exports.makeorder = function(req, res) {
+    makeorder : function (req, res) {
 //user the id if the user (we should use token to getthe id but maybe later )
 // price of the order
 // delievry fees
 // products IDs
 
-	var valid = req.body.price && validator.isInteger(req.body.price) &&
-		req.body.delivery  && validator.isInteger(req.body.delivery)
-	req.body.products && validator.isArray(req.body.products) &&  (req.body.user) && validator.isString(req.body.user) ;
+        var valid = req.body.price && validator.isInteger(req.body.price) &&
+            req.body.delivery && validator.isInteger(req.body.delivery)
+        req.body.products && validator.isArray(req.body.products) && (req.body.user) && validator.isString(req.body.user);
 
-	if (!valid) {
-		return res.status(422).json({
-			err: null,
-			msg: 'One or More field(s) is missing or of incorrect type',
-			data: null
-		});
-	}
-
-
-	let order = {
-		price: req.body.price,
-		delivery: req.body.delivery,
-		products: req.body.products,
-		user: req.body.user
-	};
+        if (!valid) {
+            return res.status(422).json({
+                err: null,
+                msg: 'One or More field(s) is missing or of incorrect type',
+                data: null
+            });
+        }
 
 
+        let order = {
+            price: req.body.price,
+            delivery: req.body.delivery,
+            products: req.body.products,
+            user: req.body.user
+        };
 
 
+        Order.create(order, function (err, neworder) {
+            if (err) {
+                return res.status(422).json({
+                    err: err,
+                    msg: "Couldn't create order",
+                    data: null
+                });
+            }
+            return res.status(200).json({
+                err: null,
+                msg: "Created order successfully",
+                data: neworder
+            });
 
-	Order.create(order, function(err, neworder) {
-		if (err) {
-			return res.status(422).json({
-				err: err,
-				msg: "Couldn't create order",
-				data: null
-			});
-		}
-		return res.status(200).json({
-			err: null,
-			msg: "Created order successfully",
-			data: neworder
-		});
-
-	});
-
+        });
 
 
+    },
 
-}
+    makeRequest : function (req, res) {
+        var valid = req.body.email &&
+            req.body.name &&
+            req.body.mob &&
+            req.body.address &&
+            req.body.description;
 
-module.exports.makeRequest = function(req,res){
-  var valid = req.body.email &&
-  req.body.name &&
-  req.body.mob &&
-  req.body.address &&
-  req.body.description;
+        if (!valid) {
+            return res.status(422).json({
+                err: null,
+                msg: 'One or More field(s) is missing or of incorrect type',
+                data: null
+            });
+        }
 
-  if (!valid) {
-    return res.status(422).json({
-      err: null,
-      msg: 'One or More field(s) is missing or of incorrect type',
-      data: null
-    });
-  }
+        let request = {
+            email: req.body.email,
+            name: req.body.name,
+            mob: req.body.mob,
+            address: req.body.address,
+            description: req.body.description
+        };
 
-  let request = {
-    email:req.body.email,
-    name:req.body.name,
-    mob:req.body.mob,
-    address:req.body.address,
-    description:req.body.description
-  };
+        Request.create(request, function (err, request) {
+            if (err) {
+                return res.status(422).json({
+                    err: err,
+                    msg: "Couldn't create request",
+                    data: null
+                });
+            }
+            return res.status(200).json({
+                err: null,
+                msg: "Created request successfully",
+                data: request
+            });
 
-  Request.create(request, function(err, request) {
-		if (err) {
-			return res.status(422).json({
-				err: err,
-				msg: "Couldn't create request",
-				data: null
-			});
-		}
-		return res.status(200).json({
-			err: null,
-			msg: "Created request successfully",
-			data: request
-		});
+        });
 
-	});
-
+    }
 }
