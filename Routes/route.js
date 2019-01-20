7acfe62164f2e5a097b2406f992d19e124f26c8d
Xsozm <hazem29778865@gmail.com>
2019-01-20 14:33:51 +0200

    Remove Hazem Route

--- Routes/route.js
+++ Routes/route.js
@@ -11,22 +11,7 @@ const UserController = require('../Controllers/UserController');
 const JSON = require('circular-json');
 var router = express.Router();
 
-router.post('/hazem',function (req, res) {
-	let name = req.body.name;
-	let description = req.body.description;
-	let properties = req.body.properties;
-	let subcategory_id = req.body.subcategory_id;
-	Product.create({"name":name,"description":description,"properties":properties,"subcategory_id":subcategory_id},function (error, product) {
-		if(error)
-		res.send(error)
-		res.send(product);
-	});
 
-
-
-
-	value
-});
 
 
 
